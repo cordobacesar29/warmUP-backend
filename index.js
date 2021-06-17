1c2a78b434f5cf96b43e8bbce43f3b2e9035e4fb
César Córdoba <cordobacesar29@gmail.com>
2021-06-17 13:07:01 -0300

    use apiRouter

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const apiRouter = require('./routes/api');
+
 require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
